0c92b77df70becffe72b0fc0c61746d05423f520
Alexander <alex.silcorp@gmail.com>
2024-02-04 19:45:01 +0100

    refactor: reuse utils function

--- src/utils/rn.js
+++ src/utils/rn.js
@@ -1,8 +1,9 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
+import isFileExist from '../helpers/isFileExist.js'
 
 async function rn (filePath, fileName) {
-  const isNotFile = !((await fs.stat(filePath)).isFile())
+  const isNotFile = !(await isFileExist(filePath))
 
   if (isNotFile) throw new Error(`Not a file: ${filePath}`)
 
